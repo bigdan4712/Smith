5e5b2db4a5739da007b86e4182b5507c4d15470b
bigdan4712 <bigdan4712@comcast.net>
2017-03-20 23:39:54 -0400

    This is the first initial commit of my OOP example of a Smith Counter used for branch prediction.  This program is for testing random input as well as reading in a .dat file for testing the smith counter algorithm.

---
+++ SmithCounter.cpp
@@ -0,0 +1,39 @@
+#include "SmithCounter.h"
+#include <cmath>
+
+SmithCounter::SmithCounter(int bits, int initialValue) {
+
+    state = initialValue;
+    number_Of_Bits = bits;
+    mask_Value = (pow(2,number_Of_Bits)) - 1;
+
+}
+void SmithCounter::record_Branch(bool taken) {
+
+    if(taken) record_Branch_Taken();
+    else record_Branch_Not_Taken();
+
+}
+void SmithCounter::record_Branch_Taken() {
+
+    state++;
+    if(state > 3) state = 3;
+
+}
+void SmithCounter::record_Branch_Not_Taken() {
+
+    state--;
+    if(state < 0) state = 0;
+
+}
+bool SmithCounter::get_Prediction() const {
+
+    if(state > 1) return true;
+    else return false;
+
+}
+int SmithCounter::get_State() const {
+
+    return state;
+
+}
---
+++ SmithCounter.h
@@ -0,0 +1,23 @@
+#ifndef SMITHCOUNTER_H
+#define SMITHCOUNTER_H
+
+
+class SmithCounter
+{
+  public:
+	SmithCounter(int bits=2, int initialValue=0);
+    void record_Branch(bool taken);
+    void record_Branch_Taken();
+    void record_Branch_Not_Taken();
+    bool get_Prediction() const;
+    int	 get_State() const;
+
+  private:
+    int number_Of_Bits;
+    int state;
+    int mask_Value;	// 2^number_Of_Bits -1
+			// a fast way to mask off unused bits
+			// set at creation
+};
+
+#endif // SMITHCOUNTER_H
